9bcbab5e598851d5a6494ec374bbddb726231910
Yushin Washio <yuwash@yandex.com>
2017-09-17 21:17:09 +0500

    add extra.sh

---
+++ extra.sh
@@ -0,0 +1,4 @@
+git config --global user.name 'Yushin Washio'
+git config --global user.email 'yuwash@yandex.com'
+mkdir -p "$HOME/ownCloud"
+ln -s /media/$USER/davfs/owndrive-yuwash "$HOME/ownCloud"
