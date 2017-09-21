b88c5d89c64824b08169efa0d994f7fdf73c49e3
Ubuntu <ubuntu@yws-home.example.com>
2017-09-21 10:18:45 +0000

    add apt-work, pip-favorites, move bash-it-dependent portion to after-bashit.sh

---
+++ after-bashit.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/env bash
+source "$HOME/.bashrc" \
+&& ( bash-it enable plugin \
+	davencmount \
+	histsync \
+	# returns 0 even if already enabled
+) \
+&& ( davencmount  # initialize
+) \
+&& (
+	davfs2path="$HOME/.davfs2"
+	mkdir -p "$davfs2path"
+	envsubst < davfs2secrets-append | tee -a "$davfs2path/secrets"
+	chmod 600 "$davfs2path/secrets"
+) \
+&& ( davencmount -l p \
+	|| vim "$HOME/.davencmount" +'normal! Gop PLEASE DEFINE FOR PRIVATE FILES'
+) \
+&& ( davencmount p  # for the private files, defined for 'p'
+) \
+&& pdavtarget="`davencmount -l p | tail -n 1 | ( read -a tmp && echo ${tmp[-1]} )`" \
+&& cp -ariT "$pdavtarget/home/Documents/SSH" "$HOME/.ssh" \
+&& chmod 600 ~/.ssh/*id_rsa \
+&& find "$pdavtarget/home/Documents/PGP" -name '*.asc' | while read key
+	do gpg --import "$key" || exit 1
+done
--- homesetup.sh
+++ homesetup.sh
@@ -6,9 +6,11 @@
 		+'%s/^XKBOPTIONS.*$/XKBOPTIONS="grp:caps_toggle,grp_led:scroll"' \
 ) \
 && udevadm trigger --subsystem-match=input --action=change \
+&& sudo apt-get update \
 && sudo apt-get install `cat \
 	apt-favorites \
 	# items below ignored \
+	apt-work \
 	apt-extras \
 	` \
 && if ! (groups | grep -q davfs2)
@@ -17,19 +19,11 @@ then
 	# not && because non-zero expected for adduser command
 	newgrp davfs2
 fi \
-&& if [[ -z "$BASH_IT" ]]
-then
-	BASH_IT="$HOME/.bash_it" \
-	&& git clone https://github.com/yuwash/bash-it.git "$BASH_IT" \
-	&& "$BASH_IT"/install.sh
-fi \
-&& vim "$HOME/.bashrc" +'%s/BASH_IT_THEME.*$'"/BASH_IT_THEME='densecandy'/" \
-&& source "$HOME/.bashrc" \
-&& ( bash-it enable plugin \
-	davencmount \
-	histsync \
-	# returns 0 even if already enabled
-) \
+&& sudo -H pip install -U pip \
+&& pip install --user `cat \
+	pip-favorites \
+	# items below ignored \
+	` \
 && if ! [[ -d /media/$USER ]]
 then
 	sudo mkdir /media/$USER \
@@ -49,29 +43,18 @@ do
 		|| mkdir -p "$target" \
 		|| exit $?
 done < fstab-append \
-&& ( davencmount  # initialize
-) \
-&& (
-	davfs2path="$HOME/.davfs2"
-	mkdir -p "$davfs2path"
-	envsubst < davfs2secrets-append | tee -a "$davfs2path/secrets"
-	chmod 600 "$davfs2path/secrets"
-) \
-&& ( davencmount -l p \
-	|| vim "$HOME/.davencmount" +'normal! Gop PLEASE DEFINE FOR PRIVATE FILES'
-) \
-&& ( davencmount p  # for the private files, defined for 'p'
-) \
-&& pdavtarget="`davencmount -l p | tail -n 1 | ( read -a tmp && echo ${tmp[-1]} )`" \
-&& cp -ariT "$pdavtarget/home/Documents/SSH" "$HOME/.ssh" \
-&& chmod 600 ~/.ssh/*id_rsa \
-&& find "$pdavtarget/home/Documents/PGP" -name '*.asc' | while read key
-	do gpg --import "$key"
-done \
 && if ! [[ -d "$HOME/.vim/bundle" ]]
 then
 	mkdir -p "$HOME/.vim/bundle" \
 	&& git clone --depth 1 https://github.com/VundleVim/Vundle.vim.git "$HOME/.vim/bundle/Vundle.vim" \
 	&& vim ~/.vim/vimrc +'r ~/.vim/bundle/Vundle.vim/README.md' +'normal! gg/^\s*```vim$"_dgg/^\s*```$"_dGgg<Ggg/^\n\s*" All of your Pluginsk"_dapOk:r vundle-favoritesG:r vimrc-append' +'wq' \
 	&& vim +PluginInstall +only
-fi
+fi \
+&& if [[ -z "$BASH_IT" ]]
+then
+	BASH_IT="$HOME/.bash_it" \
+	&& git clone https://github.com/yuwash/bash-it.git "$BASH_IT" \
+	&& "$BASH_IT"/install.sh
+fi \
+&& vim "$HOME/.bashrc" +'%s/BASH_IT_THEME.*$'"/BASH_IT_THEME='densecandy'/" \
+&& echo Please source '"$HOME/.bashrc"' and run ./after-bashit.sh
