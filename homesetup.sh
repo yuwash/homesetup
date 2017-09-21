d2576ac3ac16f2116cd1b3be53d9c5b4a4b43551
Ubuntu <ubuntu@yws-home.example.com>
2017-09-21 10:30:10 +0000

    add .tmux.conf init

--- homesetup.sh
+++ homesetup.sh
@@ -43,6 +43,16 @@ do
 		|| mkdir -p "$target" \
 		|| exit $?
 done < fstab-append \
+&& if [[ -f "$HOME/.tmux.conf" ]]
+then echo "$HOME/.tmux.conf" already exists, leaving unchanged
+else tee "$HOME/.tmux.conf" <<< 'set-option -g mode-keys vi
+set-option -g mode-keys vi
+set-option -g status-bg green
+set-window-option -g pane-active-border-style fg=green
+bind-key    -T prefix '\'\"\''              split-window -c "#{pane_current_path}"
+bind-key    -T prefix %                split-window -h -c "#{pane_current_path}"
+bind-key    -T prefix c                new-window -c "#{pane_current_path}"'
+fi \
 && if ! [[ -d "$HOME/.vim/bundle" ]]
 then
 	mkdir -p "$HOME/.vim/bundle" \
