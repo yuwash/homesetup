21ef910651ee101e3ff557e2fc4b7c8663859a72
Yushin Washio <yuwash@yandex.com>
2018-05-05 11:14:29 +0200

    apt-custominstall.sh support non-ppa reposirories with a pubkey url

--- apt-custominstall.sh
+++ apt-custominstall.sh
@@ -2,9 +2,17 @@
 
 set -ex \
 && while read package
-do PPA=`jq '.["'$package'"].ppa' < custom-sources.json` \
-	&& PPA=${PPA//\"/} \
-	&& sudo add-apt-repository ppa:$PPA
+do package_data="$(jq ".[\"$package\"]" < custom-sources.json)"
+	if [[ "true" == "$(echo $package_data | jq '. | has("ppa")')" ]]; then
+		PPA=`echo $package_data | jq --raw-output '.ppa'` \
+		&& sudo add-apt-repository ppa:$PPA
+	elif [[ "true" == "$(echo $package_data | jq '. | has("repository")')" ]]; then
+		if [[ "true" == "$(echo $package_data | jq '. | has("pubkey")')" ]]; then
+			pubkey_url="$(echo $package_data | jq --raw-output '.pubkey.url')" \
+			&& wget -qO - "$pubkey_url" | sudo apt-key add -
+		fi \
+		&& sudo add-apt-repository "$(echo $package_data | jq --raw-output '.repository')"
+	fi
 done < apt-custom \
 && sudo apt-get update \
 && sudo apt-get install `cat \
